db5bef07ba189ced43608db8a10957bbd36b21c5
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-22 21:21:46 +0300

    add validation

--- Rest-Api/src/models/UserModel.js
+++ Rest-Api/src/models/UserModel.js
@@ -1,19 +1,55 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true,
+    minLength: [6, "Username should be at least 6 characters!"],
+    valitate: {
+      validator: (value) => {
+        return /[a-zA-Z0-9_-]+/g.test(value);
+      },
+      message: (props) =>
+        `${props.value} must contains only letters, digits, underline and dash!`,
+    },
   },
   email: {
     type: String,
     required: true,
+    unique: true,
+    valitate: {
+      validator: (value) => {
+        return /[a-zA-Z0-9]{6,}@[a-zA-Z]+\.[a-z]+/g.test(value);
+      },
+      message: () => "Invalid email!",
+    },
   },
   password: {
     type: String,
     reuqired: true,
+    minLength: [6, "Password should be at least 6 characters"],
   },
+});
+
+UserSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    return bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        next(err);
+      }
+      bcrypt.hash(this.password, salt, (err, hash) => {
+        if (err) {
+          next(err);
+        }
+        this.password = hash;
+      });
+    });
+  }
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
