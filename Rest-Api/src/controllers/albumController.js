08a28a8fb66305c5388284d0dba82da7911c4889
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-23 23:47:24 +0300

    add post album functionality

--- Rest-Api/src/controllers/albumController.js
+++ Rest-Api/src/controllers/albumController.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
 
-router.get("/albums", (req, res) => {
-    
+const albumService = require("../services/albumService");
+const { trimData } = require("../utill");
 
+router.post("/albums", async (req, res) => {
+  let albumData = Object.entries(req.body);
+
+  try {
+    albumData = trimData(albumData);
+
+    const album = await albumService.createAlbum(albumData);
+
+    res.status(201).json(album);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
