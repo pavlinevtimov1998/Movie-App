8e3823494e375e4fa1abb284c3f8723a476f5f35
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-24 14:40:26 +0300

    add owner and likes props

--- Rest-Api/src/models/AlbumModel.js
+++ Rest-Api/src/models/AlbumModel.js
@@ -1,64 +1,78 @@
 const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
-const AlbumSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minLength: [5, "Album name should be at least 5 characters!"],
-    validate: {
-      validator: function (value) {
-        return /[A-Za-z]/g.test(value);
+const AlbumSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: [5, "Album name should be at least 5 characters!"],
+      validate: {
+        validator: function (value) {
+          return /[A-Za-z]/g.test(value);
+        },
+        message: () => "Album name should contains only letters!",
       },
-      message: () => "Album name should contains only letters!",
     },
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function (value) {
-        return /((https:\/\/|http:\/\/).+(\.jpg))/g.test(value);
+    imageUrl: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return /((https:\/\/|http:\/\/).+(\.jpg))/g.test(value);
+        },
+        message: () => "Album image should be valid URL!",
       },
-      message: () => "Album image should be valid URL!",
     },
-  },
-  releaseDate: {
-    type: String,
-    required: [true, "Release date is required!"],
-    validate: {
-      validator: function (value) {
-        return /[A-Za-z0-9]+/g.test(value);
+    releaseDate: {
+      type: String,
+      required: [true, "Release date is required!"],
+      validate: {
+        validator: function (value) {
+          return /[A-Za-z0-9]+/g.test(value);
+        },
+        message: () => "Invalid date!",
       },
-      message: () => "Invalid date!",
     },
-  },
-  artist: {
-    type: String,
-    required: true,
-    minLength: [5, "Artist name should be at least 5 characters!"],
-    validate: {
-      validator: function (value) {
-        return /[A-Za-z]+/g.test(value);
+    artist: {
+      type: String,
+      required: true,
+      minLength: [5, "Artist name should be at least 5 characters!"],
+      validate: {
+        validator: function (value) {
+          return /[A-Za-z]+/g.test(value);
+        },
+        message: () => "Artist name should contains only letters!",
       },
-      message: () => "Artist name should contains only letters!",
     },
-  },
-  genre: {
-    type: String,
-    required: [true, "Genre is required!"],
-    validate: {
-      validator: function (value) {
-        return /[A-Za-z]+/g.test(value);
+    genre: {
+      type: String,
+      required: [true, "Genre is required!"],
+      validate: {
+        validator: function (value) {
+          return /[A-Za-z]+/g.test(value);
+        },
+        message: () => "Genre should contains only letters!",
       },
-      message: () => "Genre should contains only letters!",
     },
+    description: {
+      type: String,
+      required: true,
+      minLength: [10, "Description should be at least 10 characters!"],
+    },
+    ownerId: {
+      type: ObjectId,
+      ref: "User",
+    },
+    likes: [
+      {
+        type: ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  description: {
-    type: String,
-    required: true,
-    minLength: [10, "Description should be at least 10 characters!"],
-  },
-});
+  { timestamps: { createdAt: "created_at" } }
+);
 
 const Album = mongoose.model("Album", AlbumSchema);
 
