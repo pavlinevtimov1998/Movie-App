73430a7732da4c42cf20d153565c423602d94e66
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-26 15:27:29 +0300

    add get user data functionality for future user profile

--- Rest-Api/src/controllers/authController.js
+++ Rest-Api/src/controllers/authController.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const { cookieName } = require("../config/constants");
+const { auth } = require("../middlewares/authMiddleware");
 const authService = require("../services/authService");
 
 router.post("/register", async (req, res) => {
@@ -12,7 +13,7 @@ router.post("/register", async (req, res) => {
       username,
       password,
     });
-    
+
     res.cookie(cookieName, token, { httpOnly: true });
 
     res.status(201).json(userData);
@@ -25,13 +26,30 @@ router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const { userData, token } = await authService.login({username, password});
+    const { userData, token } = await authService.login({ username, password });
 
     res.cookie(cookieName, token, { httpOnly: true });
 
     res.status(200).json(userData);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.get("/profile", auth, async (req, res) => {
+  const userId = res.userId;
+  try {
+    const user = await authService.getUser(userId);
+
+    if (!user) {
+      throw {
+        message: "Can't find user!",
+      };
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json(err);
   }
 });
 
