6d29cb4efd63d55ce6a99fc352117414000dabf9
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-29 13:02:51 +0300

    Create utill.ts

---
+++ Rest-Api/src/utill.ts
@@ -0,0 +1,40 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { IAlbum, IUser } from "./models/interfaces";
+
+export const removePassword = (
+  data: IUser
+): { email: string; username: string } => {
+  const { password, __v, ...user } = data;
+
+  return user;
+};
+
+export const parseDocument = (data: any) => JSON.parse(JSON.stringify(data));
+
+export const jwtPromise = (id: string, secret: string) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign({ id }, secret, { expiresIn: "1d" }, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(token);
+    });
+  });
+};
+
+export const jwtVerify = (
+  token: string,
+  secret: string
+): Promise<JwtPayload | string | undefined> => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, (err, payload) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(payload);
+    });
+  });
+};
+
+export const trimData = (data: [string, string][]) =>
+  data.reduce((a, [k, v]) => Object.assign(a, { [k]: v.trim() }), {});
