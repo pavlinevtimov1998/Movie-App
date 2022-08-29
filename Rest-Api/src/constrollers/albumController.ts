d7ad0c78bcfaf26ca54beaff5fd1e457397283e3
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-29 13:30:47 +0300

    Update albumController.ts

--- Rest-Api/src/constrollers/albumController.ts
+++ Rest-Api/src/constrollers/albumController.ts
@@ -9,18 +9,40 @@ const router = Router();
 router.post("/albums", authMiddleware, async (req: Request, res: Response) => {
   const data = Object.entries(req.body) as [string, string][];
 
-  console.log(data);
-  
   const albumData = trimData(data) as IAlbum;
+
+  if (req.userId) {
+    albumData._ownerId = req.userId;
+  }
 
   try {
     const createdAlbum = await Album.create(albumData);
 
     res.status(201).json(createdAlbum);
   } catch (err) {
-    console.log(err);
-    
     res.status(404).json(err);
+  }
+});
+
+router.get("/albums", async (req: Request, res: Response) => {
+  try {
+    const albums = await Album.find({});
+
+    res.status(200).json(albums);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get("/albums/:albumId", async (req: Request, res: Response) => {
+  const albumId = req.params.albumId;
+
+  try {
+    const album = await Album.findById({ _id: albumId });
+
+    res.status(200).json(album);
+  } catch (err) {
+    res.status(404).json({ message: "No results!" });
   }
 });
 
