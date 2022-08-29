c935cee4b1f1e500b6705d5b8578f7cac6d40121
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-29 13:03:17 +0300

    Create cors.ts

---
+++ Rest-Api/src/middlewares/cors.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+export const cors = (req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", "**");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, HEAD, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
