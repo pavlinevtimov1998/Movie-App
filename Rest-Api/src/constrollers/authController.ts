afcb29d2d9871cad3ba859ba1661d762c7513cbb
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-29 13:49:06 +0300

    Update authController.ts

--- Rest-Api/src/constrollers/authController.ts
+++ Rest-Api/src/constrollers/authController.ts
@@ -1,8 +1,11 @@
 import { Request, Response, Router } from "express";
+import bcrypt from "bcrypt";
+
 import { cookieName, sercret } from "../config/constants";
 import { IUser } from "../models/interfaces";
 import { User } from "../models/UserModel";
 import { jwtPromise, parseDocument, removePassword } from "../utill";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
@@ -26,6 +29,60 @@ router.post("/register", async (req: Request, res: Response) => {
     res.status(201).json(publicData);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body as IUser;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      throw {
+        message: "Invalid username or password!",
+      };
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+      throw {
+        message: "Invalid username or password!",
+      };
+    }
+
+    const parsedData = parseDocument(user);
+    const publicData = removePassword(parsedData);
+
+    const token = await jwtPromise(user._id, sercret);
+
+    res.cookie(cookieName, token, { httpOnly: true });
+
+    res.status(200).json(publicData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get("/profile", authMiddleware, async (req: Request, res: Response) => {
+  const userId = req.userId;
+
+  try {
+    const user = await User.findById({ _id: userId });
+
+    if (!user) {
+      throw {
+        message: "No results!",
+      };
+    }
+
+    const parsedData = parseDocument(user);
+    const publicData = removePassword(parsedData);
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json(err);
   }
 });
 
