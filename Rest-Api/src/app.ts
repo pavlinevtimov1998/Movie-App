cbd22d6e3b3dc44f23756a4be7514151766e4470
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-29 13:02:30 +0300

    Update app.ts

--- Rest-Api/src/app.ts
+++ Rest-Api/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 
-import { router } from "./routes";
+import router from "./routes";
 import { initializeDatabaze } from "./config/dbConfig";
+import { cors } from "./middlewares/cors";
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(cookieParser());
 
