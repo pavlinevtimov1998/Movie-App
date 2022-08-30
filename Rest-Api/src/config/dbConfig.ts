e4d990375e31371504aefdf784ce6b773759eb41
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-30 14:22:11 +0300

    Update dbConfig.ts

--- Rest-Api/src/config/dbConfig.ts
+++ Rest-Api/src/config/dbConfig.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
 
-const databaseUrl = "mongodb://localhost:27017/MusicApp";
-
-export const initializeDatabaze = () => mongoose.connect(databaseUrl);
+export const initializeDatabaze = (databaseUrl: string) =>
+  mongoose.connect(databaseUrl);
