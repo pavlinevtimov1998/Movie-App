c15a3085b211c48900301e07f859535f7bd2c3f6
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-30 14:22:28 +0300

    Update app.ts

--- Rest-Api/src/app.ts
+++ Rest-Api/src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 
 import router from "./routes";
 import { initializeDatabaze } from "./config/dbConfig";
 import { cors } from "./middlewares/cors";
+
+dotenv.config({ path: __dirname + "/config.env" });
+
+const port = process.env.PORT || 3000;
+const databaseUrl = process.env.DB_URL?.replace(
+  "<PASSWORD>",
+  process.env.DB_PASS || ""
+);
 
 const app = express();
 
@@ -11,11 +20,11 @@ app.use(cors);
 app.use(express.json());
 app.use(cookieParser());
 
-initializeDatabaze()
+initializeDatabaze(databaseUrl!)
   .then(() => {
     app.use(router);
 
-    app.listen(3030, () => console.log("Listening on port 3030..."));
+    app.listen(port, () => console.log(`Listening on port ${port}...`));
   })
   .catch((err) => {
     console.error(err);
