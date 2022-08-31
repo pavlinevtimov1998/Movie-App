b937c229ae0173de94d6828b0a1429540095711b
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-31 23:17:35 +0300

    add wrong url handler

--- Rest-Api/src/routes.ts
+++ Rest-Api/src/routes.ts
@@ -1,14 +1,20 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 
 const router = Router();
 
 import albumController from "./constrollers/albumController";
-import authController from './constrollers/authController'
+import authController from "./constrollers/authController";
+import { globalErrorHandler } from "./middlewares/globallErrorHandler";
+import { AppError } from "./utils/appError";
 
 router.use("/users", authController);
 
 router.use("/data", albumController);
 
-// module.exports = router;
+router.use("*", (req: Request, res: Response, next: NextFunction) => {
+  next(new AppError(`Can\'t find ${req.originalUrl} on this server!`, 404));
+});
+
+router.use(globalErrorHandler);
 
 export default router;
