cdb0e9477deec1c593b23e0806c82075aca49bd3
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-31 23:29:16 +0300

    Create catchAsyncErr.ts

---
+++ Rest-Api/src/utils/catchAsyncErr.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+
+export const catchAsyncError = (fn: Function) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res).catch(next);
+  };
+};
