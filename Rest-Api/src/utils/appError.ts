5e39af7e62d3957c5f2d5f9440744640d4bef7b3
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-01 13:58:44 +0300

    Update appError.ts

--- Rest-Api/src/utils/appError.ts
+++ Rest-Api/src/utils/appError.ts
@@ -1,6 +1,14 @@
 export class AppError extends Error {
+  isOperational: boolean;
+  status: string;
+
   constructor(public message: string, public statusCode: number) {
     super(message);
+    
+    this.status = `${this.statusCode}`.startsWith("4" || "5")
+      ? "Fail"
+      : "Success";
+    this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
   }
