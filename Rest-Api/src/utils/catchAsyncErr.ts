de38d86e0202e463c9a5a60a25642f782a4a3a67
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-01 13:58:50 +0300

    Update catchAsyncErr.ts

--- Rest-Api/src/utils/catchAsyncErr.ts
+++ Rest-Api/src/utils/catchAsyncErr.ts
@@ -3,10 +3,13 @@ import { AppError } from "./appError";
 
 export const catchAsyncError = (fn: Function) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res).catch((err: Error) => {
+    fn(req, res, next).catch((err: Error) => {
       if (err.name && err.name == "CastError") {
         return next(new AppError("Not Found!", 404));
+      } else {
+        return next(new AppError(err.message, 400));
       }
+
     });
   };
 };
