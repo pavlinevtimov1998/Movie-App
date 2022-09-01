0605a7bb67affd3e11924d766c7830c13a59c4a0
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-01 13:58:09 +0300

    Update authController.ts

--- Rest-Api/src/constrollers/authController.ts
+++ Rest-Api/src/constrollers/authController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import bcrypt from "bcrypt";
 
 import { cookieName, sercret } from "../config/constants";
@@ -7,6 +7,7 @@ import { User } from "../models/UserModel";
 import { jwtPromise, parseDocument, removePassword } from "../utils/utill";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { catchAsyncError } from "../utils/catchAsyncErr";
+import { AppError } from "../utils/appError";
 
 const router = Router();
 
@@ -14,12 +15,12 @@ router.post(
   "/register",
   catchAsyncError(async (req: Request, res: Response) => {
     const { email, username, password } = req.body as IUser;
-
+    
     const userData = await User.create({
       email,
       username,
       password,
-    });
+    });    
 
     const parsedData = parseDocument(userData);
     const publicData = removePassword(parsedData);
@@ -34,23 +35,19 @@ router.post(
 
 router.post(
   "/login",
-  catchAsyncError(async (req: Request, res: Response) => {
+  catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     const { username, password } = req.body as IUser;
 
     const user = await User.findOne({ username });
 
     if (!user) {
-      throw {
-        message: "Invalid username or password!",
-      };
+      return next(new AppError("Invalid username or password!", 401));
     }
 
     const isValid = await bcrypt.compare(password, user.password);
 
     if (!isValid) {
-      throw {
-        message: "Invalid username or password!",
-      };
+      return next(new AppError("Invalid username or password!", 401));
     }
 
     const parsedData = parseDocument(user);
@@ -67,15 +64,13 @@ router.post(
 router.get(
   "/profile",
   authMiddleware,
-  catchAsyncError(async (req: Request, res: Response) => {
+  catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.userId;
 
     const user = await User.findById({ _id: userId });
 
     if (!user) {
-      throw {
-        message: "No results!",
-      };
+      return next(new AppError("Can't user with this ID!", 404));
     }
 
     const parsedData = parseDocument(user);
