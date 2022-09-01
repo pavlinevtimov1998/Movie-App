42b812b0200f42d04fc83f9190f85a39b719399e
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-01 13:58:33 +0300

    add development and production handling

--- Rest-Api/src/middlewares/globallErrorHandler.ts
+++ Rest-Api/src/middlewares/globallErrorHandler.ts
@@ -10,6 +10,40 @@ export const globalErrorHandler = (
   err.statusCode = err.statusCode || 500;
 
   res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
     message: err.message,
+    stack: err.stack,
   });
+
+  if (process.env.NODE_ENV == "development") {
+    sendDevErr(res, err);
+  } else if (process.env.NODE_ENV == "production") {
+    sendProdErr(res, err);
+  }
+};
+
+const sendDevErr = (res: Response, err: AppError) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendProdErr = (res: Response, err: AppError) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR", err);
+
+    res.status(500).json({
+      status: "Server error!",
+      message: "Something went wrong!",
+    });
+  }
 };
