1c6a61b0d8c962d8392cd9df6d9c47b0a172ae31
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-05 13:24:50 +0300

    Update app.module.ts

--- Client/music-app/src/app/app.module.ts
+++ Client/music-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { CatalogComponent } from './album-catalog/catalog/catalog.component';
 import { AlbumItemComponent } from './album-catalog/album-item/album-item.component';
+import { CatalogService } from './album-catalog/catalog.service';
 
 @NgModule({
   declarations: [AppComponent, CatalogComponent, AlbumItemComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
+  providers: [CatalogService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
