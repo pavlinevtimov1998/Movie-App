f10edefb24ee0ef30ee05699dfabc81a24d402c6
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-05 13:24:26 +0300

    add get albums functionality

--- Client/music-app/src/app/album-catalog/catalog.service.ts
+++ Client/music-app/src/app/album-catalog/catalog.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IAlbum } from '../core/interfaces.ts/Album-Interface';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CatalogService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAlbums(): Observable<IAlbum[]> {
+    return this.http.get<IAlbum[]>('http://localhost:3000/data/albums');
+  }
 }
