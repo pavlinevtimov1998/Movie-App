6a53108a8ceed283b7aae65f003fa7e96e59682a
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-05 13:24:45 +0300

    Update catalog.component.ts

--- Client/music-app/src/app/album-catalog/catalog/catalog.component.ts
+++ Client/music-app/src/app/album-catalog/catalog/catalog.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IAlbum } from 'src/app/core/interfaces.ts/Album-Interface';
+import { CatalogService } from '../catalog.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css']
+  styleUrls: ['./catalog.component.css'],
 })
-export class CatalogComponent implements OnInit {
+export class CatalogComponent implements OnInit, OnDestroy {
+  albums!: IAlbum[];
+  subscription!: Subscription;
 
-  constructor() { }
+  constructor(private catalogService: CatalogService) {}
 
   ngOnInit(): void {
+    this.subscription = this.catalogService
+      .getAlbums()
+      .subscribe((albumsData) => {
+        this.albums = albumsData;
+      });
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
