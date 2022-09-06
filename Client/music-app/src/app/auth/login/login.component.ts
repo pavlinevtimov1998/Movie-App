f2304def7f98b33daa044907b66dacb24d31fb97
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-06 20:00:50 +0300

    add login form group

--- Client/music-app/src/app/auth/login/login.component.ts
+++ Client/music-app/src/app/auth/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup = this.formBuilder.group({
+    username: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  loginHandler() {
+    const { username, password } = this.loginForm.value;
+
+    const body = {
+      username,
+      password,
+    };
+
+    this.authService.login$(body).subscribe((data) => {
+      this.authService.currentUser = data.user;
+      this.router.navigate(['/albums/catalog']);
+    });
   }
-
 }
