a8af15cb60065c615cbc002372af7c8f5a699f09
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-06 21:09:09 +0300

    change authservice folder

--- Client/music-app/src/app/auth.service.ts
+++ Client/music-app/src/app/auth.service.ts
@@ -1,11 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { IUser } from '../core/interfaces.ts/User-interface';
+import { Observable, BehaviorSubject, map } from 'rxjs';
+
+import { IUser } from './core/interfaces.ts/User-interface';
 
 @Injectable()
 export class AuthService {
-  currentUser: IUser | undefined;
+  guest: IUser = {
+    username: '',
+  };
+
+  private _currentUser = new BehaviorSubject<IUser>(this.guest);
+
+  currentUser$ = this._currentUser.asObservable();
+  isLoggedIn$ = this.currentUser$.pipe(map((user) => user.username.length > 0));
 
   constructor(private http: HttpClient) {}
 
@@ -25,5 +33,13 @@ export class AuthService {
       userData,
       { withCredentials: true }
     );
+  }
+
+  handleLogin(user: IUser) {
+    this._currentUser.next(user);
+  }
+
+  hendleLogout() {
+    this._currentUser.next(this.guest);
   }
 }
--- Client/music-app/src/app/auth/auth.module.ts
+++ Client/music-app/src/app/auth/auth.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { AuthService } from './auth.service';
 import { authRoutingModule } from './auth-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
   imports: [CommonModule, authRoutingModule, ReactiveFormsModule],
-  providers: [AuthService],
+  providers: [],
   exports: [RegisterComponent, LoginComponent],
 })
 export class AuthModule {}
