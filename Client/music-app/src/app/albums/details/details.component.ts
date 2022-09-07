090458e32fc9dd96e484c41608aeee90ed54ef20
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-07 13:39:00 +0300

    add btns logic in details component

--- Client/music-app/src/app/albums/details/details.component.ts
+++ Client/music-app/src/app/albums/details/details.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { mergeMap, Subscription } from 'rxjs';
+import { mergeMap, Subscription, combineLatest } from 'rxjs';
+import { AuthService } from 'src/app/auth.service';
 
 import { IAlbum } from 'src/app/core/interfaces.ts/Album-Interface';
+import { IUser } from 'src/app/core/interfaces.ts/User-interface';
 import { AlbumService } from '../album.service';
 
 @Component({
@@ -14,22 +16,32 @@ export class DetailsComponent implements OnInit, OnDestroy {
   album!: IAlbum;
   subscription$!: Subscription;
   isLoading = false;
+  isLoggedIn$ = this.authService.isLoggedIn$;
+
+  currentUser!: IUser;
+  isOwner!: boolean;
 
   constructor(
     private albumService: AlbumService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private authService: AuthService
   ) {}
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.subscription$ = this.activatedRoute.params
+    this.subscription$ = combineLatest([
+      this.activatedRoute.params,
+      this.authService.currentUser$,
+    ])
       .pipe(
-        mergeMap((params) => {
+        mergeMap(([params, currentUser]) => {
           const albumId = params['albumId'];
+          this.currentUser = currentUser;
           return this.albumService.getOne(albumId);
         })
       )
       .subscribe((albumData) => {
+        this.isOwner = albumData.album._ownerId == this.currentUser._id;
         this.album = albumData.album;
         this.isLoading = false;
       });
