2cc38b278d22825bb4c8b4e9782f5d83b15a6fc5
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-07 12:54:25 +0300

    Update auth.module.ts

--- Client/music-app/src/app/auth/auth.module.ts
+++ Client/music-app/src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { authRoutingModule } from './auth-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
