995ac32fda341b6b2cf9e3700d3898e4bf1ef527
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-09 21:32:50 +0300

    get rid of unnecessary code

--- Client/music-app/src/app/app.component.ts
+++ Client/music-app/src/app/app.component.ts
@@ -8,15 +8,4 @@ import { AuthService } from './auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  isLoggedIn$ = this.authService.isLoggedIn$;
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  logout(sideNav: MatSidenav) {
-    sideNav.close();
-    this.authService.logout$().subscribe((response) => {
-      this.authService.handleLogout();
-    });
-  }
-}
+export class AppComponent {}
