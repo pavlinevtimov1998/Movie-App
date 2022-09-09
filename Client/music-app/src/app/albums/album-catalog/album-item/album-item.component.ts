09869982f237c96e6b7db2b9a37b6ddd81df3490
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-10 00:30:24 +0300

    add like functionality on each in the catalog

--- Client/music-app/src/app/albums/album-catalog/album-item/album-item.component.ts
+++ Client/music-app/src/app/albums/album-catalog/album-item/album-item.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { combineLatest, mergeMap, Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth.service';
 import { IAlbum } from 'src/app/core/interfaces.ts/Album-Interface';
+import { AlbumService } from '../../album.service';
 
 @Component({
   selector: 'app-album-item',
   templateUrl: './album-item.component.html',
   styleUrls: ['./album-item.component.css'],
 })
-export class AlbumItemComponent implements OnInit {
+export class AlbumItemComponent implements OnInit, OnDestroy {
   @Input() album!: IAlbum;
 
-  constructor() {}
+  isLoggedIn$ = this.authService.isLoggedIn$;
+  isOwner!: boolean;
+  isLiked = false;
 
-  ngOnInit(): void {}
+  subscription = new Subscription();
+
+  constructor(
+    private albumService: AlbumService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.authService.currentUser$.subscribe((user) => {
+        if (user.username.length > 0) {
+          this.isOwner = user._id == this.album._ownerId;
+        }
+
+        this.isLiked = !!this.album.likes?.find(
+          (like) => like._ownerId == user._id
+        );
+
+        console.log(this.isLiked);
+      })
+    );
+  }
+
+  likeHandler(albumId: string) {
+    this.subscription.add(
+      combineLatest([
+        this.authService.currentUser$,
+        this.albumService.likeAlbum({ albumId }),
+      ]).subscribe(([user, response]) => {
+        this.isLiked = true;
+        if (user._id) {
+          this.album.likes?.push({
+            _ownerId: user._id,
+            albumId: this.album._id,
+          });
+        }
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- Client/music-app/src/app/core/interfaces.ts/Album-Interface.ts
+++ Client/music-app/src/app/core/interfaces.ts/Album-Interface.ts
@@ -11,4 +11,5 @@ export interface IAlbum {
   description: string;
   _ownerId?: string;
   likes?: ILike[];
+  isOwner?: boolean;
 }
