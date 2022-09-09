458bf919b7709699aff654041ad568236822d660
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-09 21:13:13 +0300

    Update app.component.ts

--- Client/music-app/src/app/app.component.ts
+++ Client/music-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
+  isLoggedIn$ = this.authService.isLoggedIn$;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout(sideNav: MatSidenav) {
+    sideNav.close();
+    this.authService.logout$().subscribe((response) => {
+      this.authService.handleLogout();
+    });
+  }
 }
