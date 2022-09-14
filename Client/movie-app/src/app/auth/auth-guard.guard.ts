820975441309cf739c1452d063f0128bf63cb45f
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-14 13:46:46 +0300

    Update auth-guard.guard.ts

--- Client/movie-app/src/app/auth/auth-guard.guard.ts
+++ Client/movie-app/src/app/auth/auth-guard.guard.ts
@@ -38,7 +38,8 @@ export class AuthGuard implements CanActivate {
       return false;
     } else if (
       (route.url.find((obj) => obj.path == 'create') && !this.isLoggedIn) ||
-      (route.url.find((obj) => obj.path == 'edit') && !this.isLoggedIn)
+      (route.url.find((obj) => obj.path == 'edit') && !this.isLoggedIn) ||
+      (route.url.find((obj) => obj.path == 'profile') && !this.isLoggedIn)
     ) {
       this.subscription.unsubscribe();
       this.router.navigate(['/']);
