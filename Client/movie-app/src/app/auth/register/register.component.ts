2d6dfa7e982b927a2a620dffc26e1f98391dd316
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-09-14 21:44:50 +0300

    Update register.component.ts

--- Client/movie-app/src/app/auth/register/register.component.ts
+++ Client/movie-app/src/app/auth/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -6,7 +6,6 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 
 import { passwordMatching } from 'src/app/util/validators';
 import { AuthService } from '../../auth.service';
@@ -17,26 +16,12 @@ import { AuthService } from '../../auth.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  passwordControll = new FormControl(null, [
-    Validators.required,
-    Validators.minLength(5),
-  ]);
+  passwordControll!: FormControl;
+  registerForm!: FormGroup;
 
   get passwords(): FormGroup {
     return this.registerForm.controls['passwords'] as FormGroup;
   }
-
-  registerForm: FormGroup = this.formBuilder.group({
-    username: new FormControl(null, [Validators.required]),
-    email: new FormControl(null, [Validators.required]),
-    passwords: new FormGroup({
-      password: this.passwordControll,
-      rePass: new FormControl(null, [
-        Validators.required,
-        passwordMatching(this.passwordControll),
-      ]),
-    }),
-  });
 
   constructor(
     private authService: AuthService,
@@ -44,7 +29,24 @@ export class RegisterComponent implements OnInit {
     private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.passwordControll = new FormControl(null, [
+      Validators.required,
+      Validators.minLength(5),
+    ]);
+
+    this.registerForm = this.formBuilder.group({
+      username: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required]),
+      passwords: new FormGroup({
+        password: this.passwordControll,
+        rePass: new FormControl(null, [
+          Validators.required,
+          passwordMatching(this.passwordControll),
+        ]),
+      }),
+    });
+  }
 
   registerHandler() {
     if (this.registerForm.invalid) {
